e362f79229dfdba0c95542da29638307245dbe52
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-01-17 13:19:29 -0700

    edits

---
+++ web/src/pages/skills.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Container from '../components/container'
+import GraphQLErrorList from '../components/graphql-error-list'
+import ProjectPreviewGrid from '../components/project-preview-grid'
+import SEO from '../components/seo'
+import Layout from '../containers/layout'
+import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from '../lib/helpers'
+
+
+
+
+
+
+
+
+export default Skills
